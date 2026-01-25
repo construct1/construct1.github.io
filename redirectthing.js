e4445a9cb7bae8c41ed8354bd322da633021c2ae
101 <jlyanko76@googlemail.com>
2026-01-25 20:33:23 +0300

    Add files via upload

---
+++ redirectthing.js
@@ -0,0 +1,9 @@
+// Function to navigate to 'Patience.html'
+function goToPage() {
+    // Change the current page's URL to 'Patience.html'
+    window.location.href = "construct1.github.io/Be%20Patient.html";
+
+}
+
+
+
