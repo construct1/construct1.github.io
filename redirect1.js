ed2a0fd6b790806844af4344866b017763461f68
101 <jlyanko76@googlemail.com>
2026-01-25 20:27:33 +0300

    Update redirect1.js

--- redirect1.js
+++ redirect1.js
@@ -1,6 +1,7 @@
 // Function to navigate to 'Patience.html'
 function goToPage() {
     // Change the current page's URL to 'Patience.html'
-    window.location.href = "Be Patient.html";
+    window.location.href = "Be%20Patient.html";
 
 }
+
